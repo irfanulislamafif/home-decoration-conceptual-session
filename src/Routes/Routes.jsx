ab2adce0d62a82f7e0ca4dd6b32314f42d054c7f
irfanulislamafif <irfanulislamafif@gmail.com>
2025-12-14 19:55:26 +0600

    first commit

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,24 @@
+import { createBrowserRouter } from "react-router";
+import App from "../App";
+import Home from "../Pages/Home";
+import Products from "../Pages/Products";
+import MainLayout from "../Layouts/MainLayout";
+import ErrorPage from "../Pages/ErrorPage";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: MainLayout,
+    errorElement: <ErrorPage></ErrorPage>,
+    children: [
+      {
+        index: true,
+        Component: Home,
+      },
+      {
+        path: "/products",
+        Component: Products,
+      },
+    ],
+  },
+]);
