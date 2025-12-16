2190c2980a7fab3cde4854ec4ea1bcc6e53b4639
irfanulislamafif <irfanulislamafif@gmail.com>
2025-12-16 19:21:13 +0600

    the 6th commit

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,16 +1,19 @@
-import React from "react";
-import { Link, useLoaderData } from "react-router";
+import React, { useState } from "react";
+import { Link } from "react-router";
 import Product from "../Components/Product";
+import useProducts from "../Hooks/useProducts";
 
 const Home = () => {
-  const products = useLoaderData();
-  console.log(products);
+
+  const { products, loading, error } = useProducts();
   const featuredProducts = products.slice(0, 6);
   return (
     <div>
       <div className="flex justify-between py-5 items-center ">
         <h1 className="text-3xl font-semibold">Featured Products</h1>
-        <Link className="btn btn-outline" to="/products">See All Products</Link>
+        <Link className="btn btn-outline" to="/products">
+          See All Products
+        </Link>
       </div>
       <div className="grid gap-5 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
         {featuredProducts.map((product) => (
--- src/Pages/Products.jsx
+++ src/Pages/Products.jsx
@@ -1,11 +1,25 @@
-import React from 'react';
+import React from "react";
+import useProducts from "../Hooks/useProducts";
+import Product from "../Components/Product";
+import { Link } from "react-router";
 
 const Products = () => {
-    return (
+  const { products } = useProducts();
+  return (
         <div>
-            <h1>Products</h1>
-        </div>
-    );
+      <div className="flex justify-between py-5 items-center ">
+        <h1 className="text-3xl font-semibold">All Products</h1>
+        <Link className="btn btn-outline">
+          Search
+        </Link>
+      </div>
+      <div className="grid gap-5 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+        {products.map((product) => (
+          <Product key={product.id} product={product}></Product>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Products;
